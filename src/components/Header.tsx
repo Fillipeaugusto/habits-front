4569a5475e07e5bf87c0936957429e0ee37e9913
Fillipeaugusto <fillipeaugustoribeiro@gmail.com>
2023-01-20 23:18:27 -0300

    finish project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export function Header() {
 			<img src={LogoImg} alt="habits" />
 
 			<Dialog.Root>
-				<Dialog.Trigger className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300">
+				<Dialog.Trigger className="border transition-colors border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 focus:outline-none  focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-background">
 					<Plus size={20} className="text-violet-500" />
 					Novo hábito
 				</Dialog.Trigger>
@@ -19,7 +19,11 @@ export function Header() {
 					<Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
 					<Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 						<Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-400">
-							<X size={24} aria-label="fechar" />
+							<X
+								size={24}
+								aria-label="fechar"
+								className="rouded-lg focus:outline-none  focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
+							/>
 						</Dialog.Close>
 						<Dialog.Title className="text-3xl leading-tight font-extrabold">
 							Criar hábito
